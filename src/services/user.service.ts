3405c73121ab6f78d2b70c9b3e82a9b57dd66a9d
Dencel Cosme <miguithegreat@gmail.com>
2024-06-05 23:27:30 +0800

    chore: Refactor user service to improve code readability and maintainability

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,98 +5,103 @@ import User from "../interfaces/User";
 import bcrypt from "bcrypt";
 
 export const createUserService = async (userData: User, c_password: string) => {
-	const { given_name, family_name, user_name, email, picture, password } =
-		userData;
+  const { given_name, family_name, user_name, email, picture, password } =
+    userData;
 
-	if (password !== c_password) {
-		throw createHttpError(422, "Password does not match!");
-	}
-	const existingEmail = await UserRepository.findByEmail(String(email));
-	if (existingEmail) {
-		throw createHttpError(409, "Email already taken");
-	}
-	if (!given_name || !family_name || !email || !user_name || !password) {
-		throw createHttpError(400, "Parameters missing");
-	}
-	const existingUsername = await UserRepository.findByUsername(
-		String(user_name)
-	);
-	if (existingUsername) {
-		throw createHttpError(409, "Username already taken");
-	}
-	const passwordHashed = await bcrypt.hash(password, 10);
+  if (password !== c_password) {
+    throw createHttpError(422, "Password does not match!");
+  }
+  const existingEmail = await UserRepository.findByEmail(String(email));
+  if (existingEmail) {
+    throw createHttpError(409, "Email already taken");
+  }
+  if (!given_name || !family_name || !email || !user_name || !password) {
+    throw createHttpError(400, "Parameters missing");
+  }
+  const existingUsername = await UserRepository.findByUsername(
+    String(user_name)
+  );
+  if (existingUsername) {
+    throw createHttpError(409, "Username already taken");
+  }
+  const passwordHashed = await bcrypt.hash(password, 10);
 
-	const newUser = await UserRepository.createUser({
-		given_name,
-		family_name,
-		user_name,
-		email,
-		password: passwordHashed,
-		isFromGoogle: false,
-		picture,
-	});
+  const newUser = await UserRepository.createUser({
+    given_name,
+    family_name,
+    user_name,
+    email,
+    password: passwordHashed,
+    isFromGoogle: false,
+    picture,
+  });
 
-	return { newUser };
+  return { newUser };
 };
 
 export const getAllUserService = async (query: User) => {
-	const users = UserRepository.getAllUser(query);
-	if (!users) {
-		throw createHttpError(409, "No Users!");
-	}
-	return users;
+  const users = UserRepository.getAllUser(query);
+  if (!users) {
+    throw createHttpError(409, "No Users!");
+  }
+  return users;
 };
 
 export const updateUserService = async (userData: User, userId: string) => {
-	const { given_name, family_name, user_name, email, picture } = userData;
+  const { given_name, family_name, user_name, email, picture } = userData;
 
-	const existingEmail = await UserRepository.findByEmail(
-		String(userData.email)
-	);
+  if (!mongoose.isValidObjectId(userId)) {
+    throw createHttpError(500, "Invalid User ID");
+  }
 
-	if (existingEmail) {
-		throw createHttpError(409, "Email already taken");
-	}
+  const existingUser = await UserRepository.findById(userId);
+  if (!existingUser) {
+    throw createHttpError(404, "User not found");
+  }
 
-	const existingUsername = await UserRepository.findByUsername(
-		String(userData.user_name)
-	);
+  if (!given_name || !family_name || !email) {
+    throw createHttpError(400, "Parameters missing");
+  }
 
-	if (existingUsername) {
-		throw createHttpError(409, "Username already taken");
-	}
-	if (!mongoose.isValidObjectId(userId)) {
-		throw createHttpError(500, "Invalid User ID");
-	}
-	if (!given_name || !family_name || !email) {
-		throw createHttpError(400, "Parameters missing");
-	}
+  if (email !== existingUser.email) {
+    const existingEmail = await UserRepository.findByEmail(email);
+    if (existingEmail) {
+      throw createHttpError(409, "Email already taken");
+    }
+  }
 
-	const userUpdated = await UserRepository.updateUser(userId, {
-		given_name,
-		family_name,
-		user_name,
-		email,
-		picture,
-	});
+  if (user_name && user_name !== existingUser.user_name) {
+    const existingUsername = await UserRepository.findByUsername(user_name);
+    if (existingUsername) {
+      throw createHttpError(409, "Username already taken");
+    }
+  }
 
-	return userUpdated;
+  const userUpdated = await UserRepository.updateUser(userId, {
+    given_name,
+    family_name,
+    user_name,
+    email,
+    picture,
+  });
+
+  return userUpdated;
 };
 
 export const deleteUserService = async (userId: string) => {
-	const existingUser = UserRepository.findById(userId);
-	if (!existingUser) {
-		throw createHttpError(409, "User not found");
-	}
-	const deletedUser = UserRepository.deleteUser(userId);
-	return deletedUser;
+  const existingUser = await UserRepository.findById(userId);
+  if (!existingUser) {
+    throw createHttpError(409, "User not found");
+  }
+  const deletedUser = await UserRepository.deleteUser(userId);
+  return deletedUser;
 };
 
 export const getEnrolledCoursesService = async (userId: string) => {
-	const existingUser = UserRepository.findById(userId);
-	if (!existingUser) {
-		throw createHttpError(409, "User not found");
-	}
-	const enrolledCourses = await UserRepository.getEnrolledCourses(userId);
-	return enrolledCourses;
+  const existingUser = UserRepository.findById(userId);
+  if (!existingUser) {
+    throw createHttpError(409, "User not found");
+  }
+  const enrolledCourses = await UserRepository.getEnrolledCourses(userId);
+  return enrolledCourses;
 };
